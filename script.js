9c487e3db6bc14e5524f54de856fb28c44bc0b6e
zenzificto <140701328+zenzificto@users.noreply.github.com>
2023-10-20 17:46:04 +0100

    Update script.js
    updated JS code

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const logo = document.getElementById('logo');
+let lastScrollY = window.scrollY;
 
-window.addEventListener('scroll', function() {
-    const scrollPercentage = (window.scrollY / (document.body.scrollHeight - window.innerHeight));
+// This function will run in synchronization with the browser's repaint cycle.
+function updateRotation() {
+    const scrollPercentage = (lastScrollY / (document.body.scrollHeight - window.innerHeight));
     const rotationDegree = 360 * scrollPercentage;
     logo.style.transform = `rotate(${rotationDegree}deg)`;
-});
+    logo.style.willChange = 'transform';
+}
+
+// Debounce function
+function debounce(func, wait = 10, immediate = true) {
+    let timeout;
+    return function() {
+        const context = this, args = arguments;
+        const later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+window.addEventListener('scroll', debounce(function() {
+    lastScrollY = window.scrollY;
+    requestAnimationFrame(updateRotation);
+}));
